3ebc380cf8b20eb089f50e685e457e305f78952a
Shruthirao04 <shruthirao8042@gmail.com>
2023-06-16 15:12:23 +0530

    updated

--- src/EventSubscriber/UserRedirect.php
+++ src/EventSubscriber/UserRedirect.php
@@ -2,55 +2,61 @@
 
 namespace Drupal\shruthi_exercise\EventSubscriber;
 
-use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\HttpKernel\KernelEvents;
-use Symfony\Component\HttpKernel\Event\RequestEvent;
+// Used as baseclass for EventSubcriberDemo.
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+// Defines event for the configuration system.
+use Drupal\Core\Config\ConfigEvents;
+use Drupal\Core\Config\ConfigCrudEvent;
+use Drupal\Core\Messenger\MessengerInterface;
 
 /**
- * Class CustomConfigEvent.
- *
- * @package Drupal\shruthi_exercise\EventSubscriber
- *
- * Redirects node/add/page* to '/node/1/*'
+ * Description for class.
  */
-class UserRedirect implements EventSubscriberInterface {
+class EventsSubscriberDemo implements EventSubscriberInterface {
+  /**
+   * Extending the baseclass.
+   *
+   * @var Drupal\Core\Messenger\MessengerInterface
+   */
+  protected $messenger;
 
   /**
-   *
+   * Function.
    */
-  public function checkForRedirection(RequestEvent $event) {
-
-    // Assigns the returned request object allowing further operations to be performed on the request object using the "$request" variable.
-    $request = $event->getRequest();
-    // Assigns the URI of the request to the variable "$path.
-    $path = $request->getRequestUri();
-    // Checks the string position of the path.
-    if (strpos($path, 'node/add/page') !== FALSE) {
-      // Redirect old  urls.
-      $new_url = str_replace('node/add/page', 'node/1', $path);
-      // Redirects to the new path.
-      $new_response = new RedirectResponse($new_url, '301');
-      // Object is sent to handle the response.
-      $new_response->send();
-    }
-    // This is necessary because this also gets called on
-    // node sub-tabs such as "edit", "revisions", etc.  This
-    // prevents those pages from redirected.
-    // Checks if the current route of the request is not equal to 'entity.node.canonical'.
-    if ($request->attributes->get('_route') !== 'entity.node.canonical') {
-      return;
-    }
-
+  public function __construct(MessengerInterface $messenger) {
+    $this->messenger = $messenger;
   }
 
   /**
    * {@inheritdoc}
+   *
+   * @return array
+   *  returns array
    */
   public static function getSubscribedEvents() {
-    // The dynamic cache subscribes an event with priority 29. If you want that your code runs before that you have to use a priority >29:
-    $events[KernelEvents::REQUEST][] = ['checkForRedirection', 29];
+    // Returns the configuration when it saved.
+    $events[ConfigEvents::SAVE][] = ['configSave', -100];
+    // Returns the configuration when it is deleted.
+    $events[ConfigEvents::DELETE][] = ['configDelete', 100];
     return $events;
+  }
+
+  /**
+   * Configsave function.
+   */
+  public function configSave(ConfigCrudEvent $event) {
+    // Return the Config Object.
+    $config = $event->getConfig();
+    // Messenger service is called.
+    $this->messenger->addMessage('Saved config: ' . $config->getName());
+  }
+
+  /**
+   * Configdelete function.
+   */
+  public function configDelete(ConfigCrudEvent $event) {
+    $config = $event->getConfig();
+    $this->messenger->addMessage('Deleted config: ' . $config->getName());
   }
 
 }
