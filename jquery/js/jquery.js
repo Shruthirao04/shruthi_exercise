4696452427c60c660c629381344051d29e20857c
Shruthirao04 <shruthirao8042@gmail.com>
2023-06-26 20:34:34 +0530

    updated

--- jquery/js/jquery.js
+++ jquery/js/jquery.js
@@ -1,4 +1,4 @@
-(function ($) {
+(function ($, Drupal, drupalSettings) {
     $(document).ready(function () {
         var $first = $('#same-firstname');
         var $last = $('.form-item-lastname');
@@ -14,10 +14,9 @@
         }
         });
     });
-})(jQuery);
 
 
-(function ($, Drupal, drupalSettings) {
+
     Drupal.behaviors.MyModuleBehavior = {
         attach: function (context, settings) {
         var color_body = drupalSettings.jquery.color_body;
